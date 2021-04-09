fa1fd85150c6e2de1d93658b3e05b10faee47f02
https://github.com/Giddy324/hello-world.gitgit <giddykipngeno5@gmail.com>
2021-04-10 00:23:47 +0300

    first program first commit

---
+++ Training.py
@@ -0,0 +1,9 @@
+# Enter  First and last name
+names = input("Enter frist and last names")
+print(names)
+print()
+#Enter your goals for this class
+print("what are your goals for this class-long term goal?")
+goals =input("what would you like to be ...when all is said and done with the class?")
+print(goals)
+
